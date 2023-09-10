734de6d684c9d2d52307145f550c47776cdd6406
killman122 <91038333+killman122@users.noreply.github.com>
2023-09-10 21:46:16 +0800

    Update 秒杀.py

--- 秒杀.py
+++ 秒杀.py
@@ -117,6 +117,14 @@ def 得物商品查询(lastSpuId):
         得物商品查询(lastSpuId)
     else:
         得物商品提交(lastspu)
+        time.sleep(1)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
+        得物商品提交(lastspu)
         send_message()  # 当res_json["data"] 是 None时,执行send_message函数并结束此函数
 
 
